bb9ec0dfe35341b98866d9a938d6090a6afc8996
Guillaume Gay <guillaume@damcb.com>
2018-11-13 10:17:03 +0100

    Revert "removed temporary build files"
    This reverts commit 0f1111af45ddb757486376d60ae45d6f679a6aa3.

---
+++ mesher/build/temp.linux-x86_64-3.6/CMakeFiles/cgal_mesher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cgal_mesher.dir/src/mesher/cgal_mesher.cpp.o"
+  "../../src/mesher/cgal_mesher.pdb"
+  "../../src/mesher/cgal_mesher.cpython-36m-x86_64-linux-gnu.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cgal_mesher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
